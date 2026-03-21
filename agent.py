f8dfaef4cbf14a6f9ba7c0d02eccaa388ac647bb
notwindstone <notwindstone@gmail.com>
2026-03-21 16:23:08 +0300

    fix: fix agent.py

--- agent.py
+++ agent.py
@@ -88,7 +88,7 @@ def query_api(method, path, body=None):
         lms_api_key = config["lms_api_key"]
         url = "http://10.93.26.8:42002" + path
         headers = {
-            "Authorization": "Bearer " + config["api_key"],
+            "Authorization": "Bearer " + lms_api_key,
             "Content-Type": "application/json",
         }
         
